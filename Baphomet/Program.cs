8719605cd1e4f32b7909eddfdf3d5bd2fe6cdd93
javier orlando <javierbenitez2000@hotmail.com>
2019-11-07 17:17:04 -0400

    new project

---
+++ Baphomet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Baphomet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
